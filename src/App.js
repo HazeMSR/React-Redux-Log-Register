e75dc37d70fcf7c007d2847042eed60b834488f5
HazeMSR <hazelmsr@gmail.com>
2020-11-26 03:15:02 -0600

    Edit button

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Fragment, useEffect } from 'react';
+import SearchBar from './components/layouts/SearchBar';
+import Logs from './components/logs/Logs';
+import AddBtn from './components/layouts/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import EditLogModal from './components/logs/EditLogModal';
+import TechListModal from './components/techs/TechListModal';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import './App.css';
+
+const App = () => {
+  useEffect( () => {
+    // Init Materialize JS
+    M.AutoInit();
+  });
+
+  return (
+    <Fragment>
+
+      <SearchBar />
+        <div className='container'>
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+    </Fragment>
+  );
+}
+
+export default App;
